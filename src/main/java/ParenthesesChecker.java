82d7cd83146214af8d69fd6448df41f7a4cdc9a2
RNWalker <rebecca.noelia.walker@gmail.com>
2023-07-21 11:03:10 +0100

    working

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,8 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class ParenthesesChecker {
+    public boolean checkParentheses(String testString) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < testString.length(); i++) {
+            char x = testString.charAt(i);
+            if (x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+            } else if (x == ')' || x == ']' || x == '}') {
+                if (stack.isEmpty())
+                    return false;
+                char check = stack.pop();
+                if ((x == ')' && check != '(') ||
+                        (x == ']' && check != '[') ||
+                        (x == '}' && check != '{')) {
+                    return false;
+                }
+            }
+        }
 
-    public boolean checkParentheses(String testString){
-
+        return stack.isEmpty(); // Check if any opening brackets are left in the stack
     }
-//    need a method calls checkparentheses
-
 }
